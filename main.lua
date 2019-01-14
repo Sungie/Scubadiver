017007f2d962299fa6c70bda7ac438522fd870e8
Gelaw <securegelaw@gmail.com>
2019-01-14 08:13:18 +0100

    added more ennemies and basic ennemy generation, fixed angle rotation direction

--- main.lua
+++ main.lua
@@ -1,21 +1,21 @@
 entities = {
-  {name="ennemy1", x=1300, y=500, angle = 90, speed = 90, anglespeed=60}
+  {name="ennemy1", x=500, y=500, angle = 30, speed = 180, anglespeed=120},
+  {name="ennemy2", x=900, y=500, angle = 90, speed = 180, anglespeed=120},
+  {name="ennemy3", x=500, y=900, angle = 50, speed = 180, anglespeed=120},
+  {name="ennemy4", x=750, y=500, angle = 300, speed = 180, anglespeed=120}
 }
 
 touched = false
 shieldHandled = false
 
 function love.load()
-
   love.window.setFullscreen(true)
   width = love.graphics.getWidth()
   height = love.graphics.getHeight()
-
   shield = {name = "shield", x = width/2, y = height/2 , speed = 100, vel = nil, radius = 30}
   heart = {name = "heart", x = width/2, y = 50, width = 50, height = 50}
   table.insert(entities, shield)
   table.insert(entities, heart)
-
 end
 
 score = 0
@@ -23,12 +23,8 @@ score = 0
 function love.draw()
   love.graphics.setColor(1/score,1/score,1/score)
   love.graphics.rectangle("fill", 0,0, width, height)
-
   love.graphics.setColor(0, 0, 0)
   love.graphics.print(tostring(math.floor(score)), 100 , 100, 0,1,1)
-  love.graphics.print("angle:"..entities[1].angle, 100 , 150, 0,1,1)
-  love.graphics.print("Obj-angle:"..objectiveAngle, 100 , 200, 0,1,1)
-
   for i, entity in pairs(entities) do
     if entity.name == "heart" then
       love.graphics.setColor(1,0,0)
@@ -63,10 +59,21 @@ function love.update(dt)
   for i, entity in pairs(entities) do
     if entity.name~="heart" and entity.name~="shield" then
       objectiveAngle = 180 + math.deg(math.atan2((entity.y-heart.y),(entity.x - heart.x)))
-      entity.angle =  entity.angle + dt*entity.anglespeed*(entity.angle<objectiveAngle and 1 or -1)
+      if math.abs(entity.angle-objectiveAngle) < 180 then
+        entity.angle =  (entity.angle + dt*entity.anglespeed*(entity.angle<objectiveAngle and 1 or -1))%360
+      else
+        entity.angle =  (entity.angle + dt*entity.anglespeed*(entity.angle<objectiveAngle and -1 or 1))%360
+      end
       entity.x = entity.x+ entity.speed*math.cos(math.rad(entity.angle))*dt
       entity.y = entity.y+ entity.speed*math.sin(math.rad(entity.angle))*dt
+      if math.sqrt((math.pow((entity.x - shield.x), 2)) + (math.pow((entity.y - shield.y), 2))) < 50 then
+        table.remove(entities,i)
+      end
     end
+  end
+  if math.random(0,100)>90 then
+    local ennemy = {name="ennemy", x=math.random(0,width), y=math.random(height/2,height), angle = math.random(0,360), speed = 180, anglespeed=30}
+    table.insert(entities,ennemy)
   end
 end
 
