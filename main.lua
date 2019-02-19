f2f803dabaa0376b8be4c2f31e5c9b78403f545c
Sungie <benmanu974@gmail.com>
2019-02-19 16:41:17 +0100

    add animation

--- main.lua
+++ main.lua
@@ -118,7 +118,22 @@ function love.mousereleased(x, y, button, isTouch)
   shield:mousereleased(x,y,button,isTouch)
 end
 
+function newAnimation(image, width, height, duration)
+    local animation = {}
+    animation.spriteSheet = image;
+    animation.quads = {};
 
+    for y = 0, image:getHeight() - height, height do
+        for x = 0, image:getWidth() - width, width do
+            table.insert(animation.quads, love.graphics.newQuad(x, y, width, height, image:getDimensions()))
+        end
+    end
+
+    animation.duration = duration or 1
+    animation.currentTime = 0
+
+    return animation
+end
 --------------------------------------------------------------------------------
 function paramEnnemyGeneration()
   spawns =
